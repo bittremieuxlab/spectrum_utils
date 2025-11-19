f3c44fcc3926bc22e00053911b7c311fe3b96a59
Wout Bittremieux <bittremieux@users.noreply.github.com>
2025-11-19 10:24:59 +0100

    Change citation format

--- spectrum_utils/plot.py
+++ spectrum_utils/plot.py
@@ -297,10 +297,9 @@ def mass_errors(
 
     References
     ----------
-    Barsnes, H., Eidhammer, I. and Martens, L. (2010).
-    FragmentationAnalyzer: An open-source tool to analyze MS/MS
-    fragmentation data. PROTEOMICS, 10, 1087–1090.
-    doi:10.1002/pmic.200900681
+    Barsnes, H., Eidhammer, I. and Martens, L. FragmentationAnalyzer: An
+    open-source tool to analyze MS/MS fragmentation data. PROTEOMICS 10,
+    1087–1090 (2010). doi:10.1002/pmic.200900681
 
     """
     if ax is None:
